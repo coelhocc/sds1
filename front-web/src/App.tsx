7293cc72274199ec54b74b0ec1874980cba16753
Jean Coelho <coelhocc@gmail.com>
2020-09-16 20:12:36 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './componets/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
